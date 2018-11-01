ea6f1ab26fe0aedf6bba9d5082e0a01cb5d67f67
Kamal Saleh <ksaleh87@yahoo.com>
2018-11-01 15:21:25 +0100

    adding empty files

--- gap/ExamplesForModelCategories.gd
+++
@@ -1,9 +0,0 @@
-#
-# ExamplesForModelCategories: Package the provides many example of homotopy categoie Examples for Homotopy categories of Quillen Model categories and functors between them.
-#
-# Declarations
-#
-
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "ExamplesForModelCategories_Example" );
