3fd31d00ca37c66453c0c1d84d6210fa6b769188
Kamal Saleh <ksaleh87@yahoo.com>
2018-12-15 23:27:45 +0100

    move many methods to BBGG

--- gap/CoherentSheaves.gd
+++ gap/CoherentSheaves.gd
@@ -1,25 +1,11 @@
 
-
-DeclareAttribute( "CoherentSheavesOverProjectiveSpace", IsHomalgGradedRing );
-
-KeyDependentOperation( "TwistedStructureSheaf", IsHomalgGradedRing, IsInt, ReturnTrue );
-KeyDependentOperation( "TwistedCotangentSheaf", IsHomalgGradedRing, IsInt, ReturnTrue );
-
-DeclareOperation( "BasisBetweenTwistedStructureSheaves", [ IsHomalgGradedRing, IsInt, IsInt ] );
-DeclareOperation( "BasisBetweenTwistedCotangentSheaves", [ IsHomalgGradedRing, IsInt, IsInt ] );
-
 DeclareOperation( "TwistedStructureSheafAsQuiverRepresentation", [ IsQuiverAlgebra, IsInt, IsInt ] );
 KeyDependentOperation( "TwistedCotangentSheafAsQuiverRepresentation", IsQuiverAlgebra, IsInt, ReturnTrue );
-KeyDependentOperation( "TwistedCotangentSheafAsChain", IsHomalgGradedRing, IsInt, ReturnTrue );
 
 DeclareOperation( "BasisBetweenTwistedStructureSheavesAsQuiverRepresentations", [ IsQuiverAlgebra, IsInt, IsInt, IsInt ] );
 DeclareOperation( "BasisBetweenTwistedCotangentSheavesAsQuiverRepresentations", [ IsQuiverAlgebra, IsInt, IsInt ] );
-
-DeclareOperation( "BasisBetweenTwistedCotangentSheavesAsChains",
-[ IsHomalgGradedRing, IsInt, IsInt ] );
 
 DeclareAttribute( "CotangentBeilinsonFunctor", IsQuiverAlgebra );
 DeclareAttribute( "StructureBeilinsonFunctor", IsQuiverAlgebra );
 
 # KeyDependentOperation( "BASIS_BETWEEN_TWISTED_COTANGENT_BUNDLES_AS_CHAINS", IsHomalgGradedRing, IsInt, ReturnTrue );
-
